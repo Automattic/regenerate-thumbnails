b5dbe66a3feb269dd97c6920cbfe1c8f3ec7408e
Alex Mills <git@viper007bond.com>
2017-06-24 21:16:51 -0700

    Add dummy unit tests and Travis CI integration thanks to WP-CLI. See https://make.wordpress.org/cli/handbook/plugin-unit-tests/

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Regenerate_Thumbnails
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
