4b24ee90530e519443ef64bcca291d118a0ea617
Alex Mills <Viper007Bond@users.noreply.github.com>
2017-09-30 10:52:20 -0700

    Remove a bunch of old, unused code.

--- regenerate-thumbnails.php
+++ regenerate-thumbnails.php
@@ -94,6 +94,9 @@ class RegenerateThumbnails {
 	 * Register all of the needed hooks and actions.
 	 */
 	public function setup() {
+		// Allow people to change what capability is required to use this plugin
+		$this->capability = apply_filters( 'regenerate_thumbs_cap', $this->capability );
+
 		// Initialize the REST API routes
 		add_action( 'rest_api_init', array( $this, 'rest_api_init' ) );
 
@@ -120,9 +123,6 @@ class RegenerateThumbnails {
 
 		// Add a regenerate link to actions list in the media list view
 		add_filter( 'media_row_actions', array( $this, 'add_regenerate_link_to_media_list_view' ), 10, 2 );
-
-		// Allow people to change what capability is required to use this plugin
-		$this->capability = apply_filters( 'regenerate_thumbs_cap', $this->capability );
 	}
 
 	/**
@@ -153,7 +153,7 @@ class RegenerateThumbnails {
 		wp_enqueue_script(
 			'regenerate-thumbnails',
 			plugins_url( 'dist/build.js', __FILE__ ),
-			array(),
+			array( 'wp-api' ),
 			( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? filemtime( dirname( __FILE__ ) . '/dist/build.js' ) : $this->version,
 			true
 		);
@@ -288,41 +288,10 @@ class RegenerateThumbnails {
 	 * The main Regenerate Thumbnails interface, as displayed at Tools → Regenerate Thumbnails.
 	 */
 	public function regenerate_interface() {
-		// For testing, to use the REST API
-		var_dump( wp_create_nonce( 'wp_rest' ) );
-
 		if ( ! current_user_can( $this->capability ) ) {
 			wp_die( __( 'Cheatin&#8217; uh?' ) );
 		}
 
-		$this->regenerate_interface_vuejs_experiment();
-		return;
-
-		// This is a container for the results message that gets shown using JavaScript
-		echo '<div id="message" class="updated" style="display:none"></div>' . "\n";
-
-		// Just an overall wrapper, used to help style
-		echo '<div class="wrap regenthumbs">' . "\n";
-
-		echo '<h1>' . __( 'Regenerate Thumbnails', 'regenerate-thumbnails' ) . "</h1>\n";
-
-		// Make sure image editing is supported
-		if ( ! wp_image_editor_supports( array( 'methods' => array( 'resize' ) ) ) ) {
-			echo '<p>' . __( "Sorry but your server doesn't support image editing which means that WordPress can't create thumbnails. Please ask your host to install the Imagick or GD PHP extensions.", 'regenerate-thumbnails' ) . '</p>';
-		}
-		// Display the introduction page
-		elseif ( empty( $_POST['regenerate-thumbnails'] ) && empty( $_REQUEST['ids'] ) ) {
-			$this->regenerate_interface_introduction();
-		}
-		// Regenerate button was pushed, so start the operation
-		else {
-			$this->regenerate_interface_process();
-		}
-
-		echo '</div>'; // end "wrap regenthumbs"
-	}
-
-	public function regenerate_interface_vuejs_experiment() {
 		?>
 
 		<div class="wrap">
@@ -330,225 +299,12 @@ class RegenerateThumbnails {
 
 			<div id="regenerate-thumbnails-app">
 				<p><?php esc_html_e( 'Loading…', 'regenerate-thumbnails' ); ?></p>
+
+				<noscript><p><?php esc_html_e( 'This plugin requires that JavaScript be enabled to work.', 'regenerate-thumbnails' ); ?></p></noscript>
 			</div>
 		</div>
 
 		<?php
-	}
-
-	public function regenerate_interface_old() {
-		global $wpdb;
-
-		?>
-
-		<div id="message" class="updated fade" style="display:none"></div>
-
-		<div class="wrap regenthumbs">
-			<h2><?php _e( 'Regenerate Thumbnails', 'regenerate-thumbnails' ); ?></h2>
-
-			<?php
-
-			// If the button was clicked
-			if ( ! empty( $_POST['regenerate-thumbnails'] ) || ! empty( $_REQUEST['ids'] ) ) {
-				// Capability check
-				if ( ! current_user_can( $this->capability ) ) {
-					wp_die( __( 'Cheatin&#8217; uh?' ) );
-				}
-
-				// Create the list of image IDs
-				if ( ! empty( $_REQUEST['ids'] ) ) {
-					$images = array_map( 'intval', explode( ',', trim( $_REQUEST['ids'], ',' ) ) );
-					$ids    = implode( ',', $images );
-
-					// Form nonce check
-					check_admin_referer( $this->create_nonce_name( $images ) );
-				} else {
-					check_admin_referer( 'regenerate-thumbnails' );
-
-					// Directly querying the database is normally frowned upon, but all
-					// of the API functions will return the full post objects which will
-					// suck up lots of memory. This is best, just not as future proof.
-					if ( ! $images = $wpdb->get_results( "SELECT ID FROM $wpdb->posts WHERE post_type = 'attachment' AND post_mime_type LIKE 'image/%' ORDER BY ID DESC" ) ) {
-						echo '	<p>' . sprintf( __( "Unable to find any images. Are you sure <a href='%s'>some exist</a>?", 'regenerate-thumbnails' ), esc_url( admin_url( 'upload.php?post_mime_type=image' ) ) ) . "</p></div>";
-
-						return;
-					}
-
-					// Generate the list of IDs
-					$ids = array();
-					foreach ( $images as $image ) {
-						$ids[] = $image->ID;
-					}
-					$ids = implode( ',', $ids );
-				}
-
-
-				echo '	<p>' . __( "Please be patient while the thumbnails are regenerated. This can take a while if your server is slow (inexpensive hosting) or if you have many images. Do not navigate away from this page until this script is done or the thumbnails will not be resized. You will be notified via this page when the regenerating is completed.", 'regenerate-thumbnails' ) . '</p>';
-
-				$count = count( $images );
-
-				$text_goback     = ( ! empty( $_GET['goback'] ) ) ? sprintf( __( 'To go back to the previous page, <a href="%s">click here</a>.', 'regenerate-thumbnails' ), 'javascript:history.go(-1)' ) : '';
-				$text_failures   = sprintf( __( 'All done! %1$s image(s) were successfully resized in %2$s seconds and there were %3$s failure(s). To try regenerating the failed images again, <a href="%4$s">click here</a>. %5$s', 'regenerate-thumbnails' ), "' + rt_successes + '", "' + rt_totaltime + '", "' + rt_errors + '", esc_url( wp_nonce_url( admin_url( 'tools.php?page=regenerate-thumbnails&goback=1' ), 'regenerate-thumbnails' ) . '&ids=' ) . "' + rt_failedlist + '", $text_goback );
-				$text_nofailures = sprintf( __( 'All done! %1$s image(s) were successfully resized in %2$s seconds and there were 0 failures. %3$s', 'regenerate-thumbnails' ), "' + rt_successes + '", "' + rt_totaltime + '", $text_goback );
-				?>
-
-
-				<noscript><p><em><?php _e( 'You must enable Javascript in order to proceed!', 'regenerate-thumbnails' ) ?></em></p></noscript>
-
-				<div id="regenthumbs-bar" style="position:relative;height:25px;">
-					<div id="regenthumbs-bar-percent" style="position:absolute;left:50%;top:50%;width:300px;margin-left:-150px;height:25px;margin-top:-9px;font-weight:bold;text-align:center;"></div>
-				</div>
-
-				<p><input type="button" class="button hide-if-no-js" name="regenthumbs-stop" id="regenthumbs-stop" value="<?php _e( 'Abort Resizing Images', 'regenerate-thumbnails' ) ?>" /></p>
-
-				<h3 class="title"><?php _e( 'Debugging Information', 'regenerate-thumbnails' ) ?></h3>
-
-				<p>
-					<?php printf( __( 'Total Images: %s', 'regenerate-thumbnails' ), $count ); ?><br />
-					<?php printf( __( 'Images Resized: %s', 'regenerate-thumbnails' ), '<span id="regenthumbs-debug-successcount">0</span>' ); ?><br />
-					<?php printf( __( 'Resize Failures: %s', 'regenerate-thumbnails' ), '<span id="regenthumbs-debug-failurecount">0</span>' ); ?>
-				</p>
-
-				<ol id="regenthumbs-debuglist">
-					<li style="display:none"></li>
-				</ol>
-
-				<script type="text/javascript">
-					jQuery(document).ready(function ($) {
-						var i;
-						var rt_images = [<?php echo $ids; ?>];
-						var rt_total = rt_images.length;
-						var rt_count = 1;
-						var rt_percent = 0;
-						var rt_successes = 0;
-						var rt_errors = 0;
-						var rt_failedlist = '';
-						var rt_resulttext = '';
-						var rt_timestart = new Date().getTime();
-						var rt_timeend = 0;
-						var rt_totaltime = 0;
-						var rt_continue = true;
-
-						// Create the progress bar
-						$("#regenthumbs-bar").progressbar();
-						$("#regenthumbs-bar-percent").html("0%");
-
-						// Stop button
-						$("#regenthumbs-stop").click(function () {
-							rt_continue = false;
-							$('#regenthumbs-stop').val("<?php echo $this->esc_quotes( __( 'Stopping...', 'regenerate-thumbnails' ) ); ?>");
-						});
-
-						// Clear out the empty list element that's there for HTML validation purposes
-						$("#regenthumbs-debuglist li").remove();
-
-						// Called after each resize. Updates debug information and the progress bar.
-						function RegenThumbsUpdateStatus(id, success, response) {
-							$("#regenthumbs-bar").progressbar("value", ( rt_count / rt_total ) * 100);
-							$("#regenthumbs-bar-percent").html(Math.round(( rt_count / rt_total ) * 1000) / 10 + "%");
-							rt_count = rt_count + 1;
-
-							if (success) {
-								rt_successes = rt_successes + 1;
-								$("#regenthumbs-debug-successcount").html(rt_successes);
-								$("#regenthumbs-debuglist").append("<li>" + response.success + "</li>");
-							}
-							else {
-								rt_errors = rt_errors + 1;
-								rt_failedlist = rt_failedlist + ',' + id;
-								$("#regenthumbs-debug-failurecount").html(rt_errors);
-								$("#regenthumbs-debuglist").append("<li>" + response.error + "</li>");
-							}
-						}
-
-						// Called when all images have been processed. Shows the results and cleans up.
-						function RegenThumbsFinishUp() {
-							rt_timeend = new Date().getTime();
-							rt_totaltime = Math.round(( rt_timeend - rt_timestart ) / 1000);
-
-							$('#regenthumbs-stop').hide();
-
-							if (rt_errors > 0) {
-								rt_resulttext = '<?php echo $text_failures; ?>';
-							} else {
-								rt_resulttext = '<?php echo $text_nofailures; ?>';
-							}
-
-							$("#message").html("<p><strong>" + rt_resulttext + "</strong></p>");
-							$("#message").show();
-						}
-
-						// Regenerate a specified image via AJAX
-						function RegenThumbs(id) {
-							$.ajax({
-								type   : 'POST',
-								url    : ajaxurl,
-								data   : {action: "regeneratethumbnail", id: id},
-								success: function (response) {
-									if (response === null) {
-										response = new Object;
-										response.success = false;
-										response.error = "The resize request was abnormally terminated (ID " + id + "). This is likely due to the image exceeding available memory.";
-									}
-
-									if (response.success) {
-										RegenThumbsUpdateStatus(id, true, response);
-									}
-									else {
-										RegenThumbsUpdateStatus(id, false, response);
-									}
-
-									if (rt_images.length && rt_continue) {
-										RegenThumbs(rt_images.shift());
-									}
-									else {
-										RegenThumbsFinishUp();
-									}
-								},
-								error  : function (response) {
-									RegenThumbsUpdateStatus(id, false, response);
-
-									if (rt_images.length && rt_continue) {
-										RegenThumbs(rt_images.shift());
-									}
-									else {
-										RegenThumbsFinishUp();
-									}
-								}
-							});
-						}
-
-						RegenThumbs(rt_images.shift());
-					});
-				</script>
-			<?php
-			}
-
-			// No button click? Display the form.
-			else {
-			?>
-				<form method="post" action="">
-					<?php wp_nonce_field( 'regenerate-thumbnails' ) ?>
-
-					<p><?php printf( __( "Use this tool to regenerate thumbnails for all images that you have uploaded to your site. This is useful if you've changed any of the thumbnail dimensions on the <a href='%s'>media settings page</a> or switched themes. Old thumbnails will be kept to avoid any broken images due to hard-coded URLs.", 'regenerate-thumbnails' ), esc_url( admin_url( 'options-media.php' ) ) ); ?></p>
-
-					<p><?php printf( __( "You can regenerate specific images (rather than all images) from the <a href='%s'>Media</a> page. Hover over an image's row and click the link to resize just that one image or use the checkboxes and the &quot;Bulk Actions&quot; dropdown to resize multiple images (WordPress 3.1+ only).", 'regenerate-thumbnails' ), esc_url( admin_url( 'upload.php' ) ) ); ?></p>
-
-					<p><?php _e( "Thumbnail regeneration is not reversible, but you can just change your thumbnail dimensions back to the old values and click the button again if you don't like the results.", 'regenerate-thumbnails' ); ?></p>
-
-					<p><?php _e( 'To begin, just press the button below.', 'regenerate-thumbnails' ); ?></p>
-
-					<p><input type="submit" class="button hide-if-no-js" name="regenerate-thumbnails" id="regenerate-thumbnails" value="<?php _e( 'Regenerate All Thumbnails', 'regenerate-thumbnails' ) ?>" /></p>
-
-					<noscript><p><em><?php _e( 'You must enable Javascript in order to proceed!', 'regenerate-thumbnails' ) ?></em></p></noscript>
-
-				</form>
-			<?php
-			} // End if button
-			?>
-		</div>
-
-	<?php
 	}
 
 	/**
@@ -608,11 +364,6 @@ class RegenerateThumbnails {
 		echo "</p>\n";
 	}
 
-
-	public function regenerate_interface_process() {
-		var_dump( $_POST );
-	}
-
 	/**
 	 * Returns an array of all thumbnail sizes, including their label, size, and crop setting.
 	 *
@@ -637,69 +388,6 @@ class RegenerateThumbnails {
 
 		return $thumbnail_sizes;
 	}
-
-	/**
-	 * AJAX handler that regenerates the thumbnails a single image attachment. Outputs JSON.
-	 */
-	public function ajax_process_image() {
-		@error_reporting( 0 ); // Don't break the JSON result
-
-		header( 'Content-type: application/json' );
-
-		$id    = (int) $_REQUEST['id'];
-		$image = get_post( $id );
-
-		if ( ! $image || 'attachment' != $image->post_type || 'image/' != substr( $image->post_mime_type, 0, 6 ) ) {
-			die( json_encode( array( 'error' => sprintf( __( 'Failed resize: %s is an invalid image ID.', 'regenerate-thumbnails' ), esc_html( $_REQUEST['id'] ) ) ) ) );
-		}
-
-		if ( ! current_user_can( $this->capability ) ) {
-			$this->die_json_error_msg( $image->ID, __( "Your user account doesn't have permission to resize images", 'regenerate-thumbnails' ) );
-		}
-
-		$fullsizepath = get_attached_file( $image->ID );
-
-		if ( false === $fullsizepath || ! file_exists( $fullsizepath ) ) {
-			$this->die_json_error_msg( $image->ID, sprintf( __( 'The originally uploaded image file cannot be found at %s', 'regenerate-thumbnails' ), '<code>' . esc_html( $fullsizepath ) . '</code>' ) );
-		}
-
-		@set_time_limit( 900 ); // 5 minutes per image should be PLENTY
-
-		$metadata = wp_generate_attachment_metadata( $image->ID, $fullsizepath );
-
-		if ( is_wp_error( $metadata ) ) {
-			$this->die_json_error_msg( $image->ID, $metadata->get_error_message() );
-		}
-		if ( empty( $metadata ) ) {
-			$this->die_json_error_msg( $image->ID, __( 'Unknown failure reason.', 'regenerate-thumbnails' ) );
-		}
-
-		// If this fails, then it just means that nothing was changed (old value == new value)
-		wp_update_attachment_metadata( $image->ID, $metadata );
-
-		die( json_encode( array( 'success' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) was successfully resized in %3$s seconds.', 'regenerate-thumbnails' ), esc_html( get_the_title( $image->ID ) ), $image->ID, timer_stop() ) ) ) );
-	}
-
-	/**
-	 * Create a JSON error message and die.
-	 *
-	 * @param int    $id      The attachment ID.
-	 * @param string $message The error message.
-	 */
-	public function die_json_error_msg( $id, $message ) {
-		die( json_encode( array( 'error' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) failed to resize. The error message was: %3$s', 'regenerate-thumbnails' ), esc_html( get_the_title( $id ) ), $id, $message ) ) ) );
-	}
-
-	/**
-	 * Slash escape double quotes.
-	 *
-	 * @param string $string The string to escape.
-	 *
-	 * @return string The escaped string.
-	 */
-	public function esc_quotes( $string ) {
-		return str_replace( '"', '\"', $string );
-	}
 }
 
 /**
@@ -712,6 +400,6 @@ function RegenerateThumbnails() {
 }
 
 /**
- * Initalize this plugin once all other plugins have finished loading.
+ * Initialize this plugin once all other plugins have finished loading.
  */
 add_action( 'plugins_loaded', 'RegenerateThumbnails' );
