d4549b0bf16f1faee61252d0ab31626f0a080bd5
Alex Mills <Viper007Bond@users.noreply.github.com>
2017-10-15 16:13:20 -0700

    Wait until everything is fully loaded before processing attachments.
    [ci skip]

--- src/components/RegenerateAll.vue
+++ src/components/RegenerateAll.vue
@@ -15,13 +15,14 @@
 			}
 		},
 		mounted   : function () {
-			let processed = 0;
-			let page = 1;
-			let total = 0;
-			let totalPages = 0;
+			this.$nextTick(function () {
+				let processed = 0;
+				let page = 1;
+				let total = 0;
+				let totalPages = 0;
 
-			do {
-				wp.apiRequest({
+				do {
+					wp.apiRequest({
 						namespace: 'wp/v2',
 						endpoint : 'media?_fields=id,title',
 						data     : {
@@ -36,38 +37,39 @@
 						async    : false,
 						context  : this
 					})
-					.done((attachments, textStatus, jqXHR) => {
-						total = jqXHR.getResponseHeader('x-wp-total');
-						totalPages = jqXHR.getResponseHeader('x-wp-totalpages');
+						.done((attachments, textStatus, jqXHR) => {
+							total = jqXHR.getResponseHeader('x-wp-total');
+							totalPages = jqXHR.getResponseHeader('x-wp-totalpages');
 
-						attachments.forEach(attachment => {
-							wp.apiRequest({
-								namespace: 'regenerate-thumbnails/v1',
-								endpoint : 'attachmentinfo/' + attachment.id,
-								type     : 'GET',
-								dataType : 'json',
-								async    : false,
-								context  : this
-							})
-							.done((attachment, textStatus, jqXHR) => {
-								processed++;
-								this.progress = Math.round((processed / total) * 100);
+							attachments.forEach(attachment => {
+								wp.apiRequest({
+									namespace: 'regenerate-thumbnails/v1',
+									endpoint : 'attachmentinfo/' + attachment.id,
+									type     : 'GET',
+									dataType : 'json',
+									async    : false,
+									context  : this
+								})
+									.done((attachment, textStatus, jqXHR) => {
+										processed++;
+										this.progress = Math.round((processed / total) * 100);
 
-								console.log(attachment);
-							})
-							.fail(function (error) {
-								processed++;
-								console.log('ERROR!', error);
+										console.log(attachment);
+									})
+									.fail(function (error) {
+										processed++;
+										console.log('ERROR!', error);
+									});
 							});
+						})
+						.fail(function (error) {
+							console.log(error);
 						});
-					})
-					.fail(function (error) {
-						console.log(error);
-					});
 
-				page++;
-			}
-			while (page <= totalPages) ;
+					page++;
+				}
+				while (page <= totalPages);
+			});
 		},
 		components: {
 			ProgressBar,
