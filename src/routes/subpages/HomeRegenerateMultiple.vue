a5befb829fa7b6c3bd0a11123754daf7a9dc5cc5
Alex Mills <Viper007Bond@users.noreply.github.com>
2018-01-28 00:28:48 -0800

    If the AJAX response is null, throw an error. Fixes #59.
    Also abstracts the error logging code out into a function.

--- src/routes/subpages/HomeRegenerateMultiple.vue
+++ src/routes/subpages/HomeRegenerateMultiple.vue
@@ -157,6 +157,11 @@
 					context  : vue,
 				})
 					.done((result, textStatus, jqXHR) => {
+						if ( null === result ) {
+							logError( vue, attachment.id, jqXHR, textStatus, result );
+							return;
+						}
+
 						// Make the attachment name clickable
 						if ( result.edit_url !== null ) {
 							let a = document.createElement('a');
@@ -171,56 +176,7 @@
 						});
 					})
 					.fail((jqXHR, textStatus, errorThrown) => {
-						console.log('Regenerate Thumbnails: Error while trying to regenerate attachment ID ' + attachment.id, jqXHR, textStatus, errorThrown);
-
-						let item = {};
-
-						// Make the attachment ID clickable
-						let a = document.createElement('a');
-						a.href = vue.regenerateThumbnails.data.genericEditURL + attachment.id;
-						a.textContent = attachment.id;
-						let clickableID = a.outerHTML;
-
-						if (
-							jqXHR !== null &&
-							jqXHR.hasOwnProperty('responseJSON') &&
-							jqXHR.responseJSON !== null
-						) {
-							if (
-								jqXHR.responseJSON.hasOwnProperty('data') &&
-								jqXHR.responseJSON.data !== null &&
-								jqXHR.responseJSON.data.hasOwnProperty('attachment') &&
-								jqXHR.responseJSON.data.attachment !== null
-							) {
-								item = {
-									id     : jqXHR.responseJSON.data.attachment.ID,
-									message: vue.regenerateThumbnails.l10n.RegenerateMultiple.logSkippedItem.formatUnicorn({
-										id    : clickableID,
-										name  : jqXHR.responseJSON.data.attachment.post_title,
-										reason: jqXHR.responseJSON.message,
-									}),
-								};
-							} else {
-								item = {
-									id     : attachment.id,
-									message: vue.regenerateThumbnails.l10n.RegenerateMultiple.logSkippedItemNoName.formatUnicorn({
-										id    : clickableID,
-										reason: jqXHR.responseJSON.message,
-									}),
-								};
-							}
-						} else {
-							item = {
-								id     : attachment.id,
-								message: vue.regenerateThumbnails.l10n.RegenerateMultiple.logSkippedItemNoName.formatUnicorn({
-									id    : clickableID,
-									reason: errorThrown,
-								}),
-							};
-						}
-
-						vue.logItems.push(item);
-						vue.errorItems.push(item);
+						logError( vue, attachment.id, jqXHR, textStatus, errorThrown );
 					})
 					.always(() => {
 						processed++;
@@ -268,6 +224,59 @@
 							processChunkOfAttachments();
 						}
 					});
+			}
+
+			function logError( vue, attachmentID, jqXHR, textStatus, errorThrown ) {
+				console.log('Regenerate Thumbnails: Error while trying to regenerate attachment ID ' + attachmentID, jqXHR, textStatus, errorThrown);
+
+				let item = {};
+
+				// Make the attachment ID clickable
+				let a = document.createElement('a');
+				a.href = vue.regenerateThumbnails.data.genericEditURL + attachmentID;
+				a.textContent = attachmentID;
+				let clickableID = a.outerHTML;
+
+				if (
+					jqXHR !== null &&
+					jqXHR.hasOwnProperty('responseJSON') &&
+					jqXHR.responseJSON !== null
+				) {
+					if (
+						jqXHR.responseJSON.hasOwnProperty('data') &&
+						jqXHR.responseJSON.data !== null &&
+						jqXHR.responseJSON.data.hasOwnProperty('attachment') &&
+						jqXHR.responseJSON.data.attachment !== null
+					) {
+						item = {
+							id     : jqXHR.responseJSON.data.attachment.ID,
+							message: vue.regenerateThumbnails.l10n.RegenerateMultiple.logSkippedItem.formatUnicorn({
+								id    : clickableID,
+								name  : jqXHR.responseJSON.data.attachment.post_title,
+								reason: jqXHR.responseJSON.message,
+							}),
+						};
+					} else {
+						item = {
+							id     : attachmentID,
+							message: vue.regenerateThumbnails.l10n.RegenerateMultiple.logSkippedItemNoName.formatUnicorn({
+								id    : clickableID,
+								reason: jqXHR.responseJSON.message,
+							}),
+						};
+					}
+				} else {
+					item = {
+						id     : attachmentID,
+						message: vue.regenerateThumbnails.l10n.RegenerateMultiple.logSkippedItemNoName.formatUnicorn({
+							id    : clickableID,
+							reason: errorThrown,
+						}),
+					};
+				}
+
+				vue.logItems.push(item);
+				vue.errorItems.push(item);
 			}
 
 			window.onbeforeunload = undefined;
